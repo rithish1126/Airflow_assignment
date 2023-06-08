f501c59c52a7b39dd0c5475159e657c1d976ff88
rithish1126 <122535424+rithish1126@users.noreply.github.com>
2023-06-08 12:35:55 +0530

    Update question2.py

--- dags/question2.py
+++ dags/question2.py
@@ -1,14 +1,13 @@
 from airflow import DAG 
 from datetime import datetime
-from airflow.operators.python_operator import PythonOperator 
+from airflow.operators.dummy_operator import DummyOperator 
 from airflow.operators.email_operator import EmailOperator
 
-def start():# Dummy task that runs this function before sending the mail
-    print("start")
+
 with DAG(dag_id="mail",start_date=datetime(2023,4,22),schedule_interval="@daily") as dag:
-    dummytask=PythonOperator( #Dummy task that runs the above function
+    dummytask=DummyOperator( #Dummy task that runs the above function
         task_id="dummytask",
-        python_callable=start
+        
     )
      
     sendmail = EmailOperator( #Send Mail task that sends the mail to the specified mail using the EmailOperator
